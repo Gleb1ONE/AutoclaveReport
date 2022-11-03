5aa155834371c5a5f8884a6c7161719becd5e3d4
Gleb1ONE <88017269+Gleb1ONE@users.noreply.github.com>
2022-11-03 23:44:20 +0500

    Add interface for program

--- main.py
+++ main.py
@@ -1,43 +1,44 @@
 import matplotlib.pyplot as plt
 
-doc_input = open("graphic.csv")
 
-file = doc_input.read()
-doc_input.close()
+def __init__():
+    time_graph = []
+    temperature_graph = []
 
-time_graph = []
-temperature_graph = []
 
-list = file.replace(';','\n')
-list = list.splitlines()
+def openFile(file):
+    #Открытие CSV файла
+    doc_input = open(file)
+    file = doc_input.read()     #чтение файла в переменную
+    doc_input.close()
 
-for x in range(len(list)):
-    if x % 2 == 0:
-        time_graph.append(list[x])
-    else:
-        temperature_graph.append(list[x])
 
-#print(time_graph)
-print(temperature_graph)
+    list = file.replace(';','\n')   #Замена ';' на переносы строк
+    list = list.splitlines()    # Разбиение list по переносу
 
-time_graph.pop(0)
-temperature_graph.pop(0)
 
-for i in range(len(temperature_graph)):
-    temperature_graph[i] = temperature_graph[i].replace(',', '.')
-    temperature_graph[i] = float(temperature_graph[i])
+    for x in range(len(list)):  #Разброс температуры и времени по четности
+        if x % 2 == 0:
+            time_graph.append(list[x])
+        else:
+            temperature_graph.append(list[x])
 
-print(temperature_graph)
+    time_graph.pop(0)       #Удаление заголовка
+    temperature_graph.pop(0)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+    for i in range(len(temperature_graph)): #Перевод температуры в формат с плавающей точкой
+        temperature_graph[i] = temperature_graph[i].replace(',', '.')
+        temperature_graph[i] = float(temperature_graph[i])
 
-#ax.set_xlim([-10, 10])
-ax.set_ylim([10, 150])
-ax.set_title('График')
-ax.set_xlabel('Время')
-ax.set_ylabel('Температура')
+def initGraph(time, temperature):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
 
-ax.plot(time_graph, temperature_graph)  # Plot some data on the axes.
+    #ax.set_xlim([-10, 10])
+    ax.set_ylim([10, 150])
+    ax.set_title('График')
+    ax.set_xlabel('Время')
+    ax.set_ylabel('Температура')
 
-plt.show()
+    ax.plot(time, temperature)
+    plt.show()

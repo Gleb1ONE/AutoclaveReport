4498de782173b98c17f49aaf61c3211957cf6ce0
Gleb1ONE <88017269+Gleb1ONE@users.noreply.github.com>
2022-11-05 23:46:06 +0500

    Add graph on prog

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 from design import report_prog
 import sys
+#---------------------------------------------------------------------------
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+import random
+#---------------------------------------------------------------------------
 
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
@@ -9,17 +16,44 @@ class Window(QtWidgets.QMainWindow):
         self.ui = report_prog.Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.ui.pushButton_1.clicked.connect(self.btnOpen)
+        self.ui.pushButton.clicked.connect(self.btnOpen)
         self.ui.pushButton_2.clicked.connect(self.btnSave)
+    # ---------------------------------------------------------------------------
+        self.ui.figure = plt.figure()
+        self.ui.canvas = FigureCanvas(self.ui.figure)
+        self.ui.toolbar = NavigationToolbar(self.ui.canvas, self)
+        layout = QVBoxLayout()
+        layout.addWidget(self.ui.toolbar)
+        layout.addWidget(self.ui.canvas)
+
+        self.ui.widget.setLayout(layout)
 
 
     def btnOpen(self):
         fname = QFileDialog.getOpenFileName(self, 'Открыть файл', '/home', '*.csv;;')[0]
         print(fname)
+        self.plot()
 
     def btnSave(self):
         fname = QFileDialog.getSaveFileName(self, 'Открыть файл', '/home', '*.pdf')[0]
         print(fname)
+
+    def plot(self):
+        # random data
+        data = [random.random() for i in range(10)]
+
+        # clearing old figure
+        self.ui.figure.clear()
+
+        # create an axis
+        ax = self.ui.figure.add_subplot(111)
+
+        # plot data
+        ax.plot(data, '*-')
+
+        # refresh canvas
+        self.ui.canvas.draw()
+
 
 
 app = QtWidgets.QApplication([])
